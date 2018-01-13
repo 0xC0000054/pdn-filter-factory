fd5cffae845f3ac6ed866840d4ebedfb193c9ab6
0xC0000054 <0xC0000054@users.noreply.github.com>
2018-01-12 21:37:46 -0700

    Add the file header

--- filterdata.cs
+++ filterdata.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿/*
+*  This file is part of pdn-filter-factory, a Paint.NET Effect that
+*  interprets Filter Factory-based Adobe Photoshop filters.
+*
+*  Copyright (C) 2010, 2011, 2012, 2015, 2018 Nicholas Hayes
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+using System;
 
 namespace PdnFF
 {
