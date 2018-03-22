0fab103c38aa79d8568cf6aecda10e82b9b402c2
0xC0000054 <0xC0000054@users.noreply.github.com>
2018-03-22 08:55:58 -0600

    Release version 1.0.4.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -55,5 +55,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.3.7")]
-[assembly: AssemblyFileVersion("1.0.3.7")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
